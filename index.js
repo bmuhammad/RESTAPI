6a5c971e159ac7757503f2e4d9adc20fe8eff98f
Brian <briancmuhammad@yahoo.com>
2024-09-27 10:27:37 -0700

    testing for vercel deploy

--- index.js
+++ index.js
@@ -61,11 +61,20 @@ router.get("/:id", function (req, res, next) {
 });
 
 //Configure router so all routes are prefixed with /api/v1
-app.use("/api/", router);
+//app.use("/api/", router);
+//test for vercel deploy
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
+  
+  // Initialize server
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
+  });
 
 //Create server to listen on port 5000
-var server = app.listen(5000, function () {
-  console.log("Node server is running on http://localhose:5000..");
-});
+// var server = app.listen(5000, function () {
+//   console.log("Node server is running on http://localhose:5000..");
+// });
 
-module.exports = server;
+//module.exports = server;
