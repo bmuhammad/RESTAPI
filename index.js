ddfcd219ce065029b60ba354495b6d8cc6c6c45e
Brian <briancmuhammad@yahoo.com>
2024-09-27 12:30:51 -0700

    daljflajkjfj

--- index.js
+++ index.js
@@ -30,35 +30,35 @@ router.get("/", function (req, res, next) {
 });
 
 //Create GET/id to return a single satellite
-router.get("/:id", function (req, res, next) {
-  satelliteRepo.getById(
-    req.params.id,
-    function (data) {
-      if (data) {
-        res.status(200).json({
-          status: 200,
-          statusText: "OK",
-          message: "All satellites retrieved.",
-          data: data,
-        });
-      } else {
-        res.status(200).json({
-          status: 404,
-          statusText: ",Not Found",
-          message: "The satellite '" + req.params.id + "' could not be found.",
-          error: {
-            code: "NOT_FOUND",
-            message:
-              "The satellite '" + req.params.id + "' could not be found.",
-          },
-        });
-      }
-    },
-    function (err) {
-      next(err);
-    }
-  );
-});
+// router.get("/:id", function (req, res, next) {
+//   satelliteRepo.getById(
+//     req.params.id,
+//     function (data) {
+//       if (data) {
+//         res.status(200).json({
+//           status: 200,
+//           statusText: "OK",
+//           message: "All satellites retrieved.",
+//           data: data,
+//         });
+//       } else {
+//         res.status(200).json({
+//           status: 404,
+//           statusText: ",Not Found",
+//           message: "The satellite '" + req.params.id + "' could not be found.",
+//           error: {
+//             code: "NOT_FOUND",
+//             message:
+//               "The satellite '" + req.params.id + "' could not be found.",
+//           },
+//         });
+//       }
+//     },
+//     function (err) {
+//       next(err);
+//     }
+//   );
+// });
 
 //Configure router so all routes are prefixed with /api/v1
 //app.use("/api/", router);
