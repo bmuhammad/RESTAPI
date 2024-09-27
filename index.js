7efe5b0c78856b2119ff020b83bbed7bd3869e01
Brian <briancmuhammad@yahoo.com>
2024-09-27 14:07:15 -0700

    simpl

--- index.js
+++ index.js
@@ -5,90 +5,13 @@ let router = express.Router();
 let app = express();
 let satelliteRepo = require("./repos/satelliteRepo");
 
-
-//possible sql  call for data
 let satellites = satelliteRepo.get();
 
-//Create GET to return a list of items
-// router.get("/", function (req, res, next) {
-//   // res.send("Communication"); return on single piece of data
-//   //can send status here res.status(206).send based on needs
-//   /// res.send(satellites);
-
-//   satelliteRepo.get(
-//     function (data) {
-//       res.status(200).json({
-//         status: 200,
-//         statusText: "OK",
-//         message: "All satellites retrieved.",
-//         data: data,
-//       });
-//     },
-//     function (err) {
-//       next(err);
-//     }
-//   );
-// });
-
 router.get("/", function (req, res, next) {
-   
-
     res.send(satellites);
-
- 
     
   });
 
-//Create GET/id to return a single satellite
-// router.get("/:id", function (req, res, next) {
-//   satelliteRepo.getById(
-//     req.params.id,
-//     function (data) {
-//       if (data) {
-//         res.status(200).json({
-//           status: 200,
-//           statusText: "OK",
-//           message: "All satellites retrieved.",
-//           data: data,
-//         });
-//       } else {
-//         res.status(200).json({
-//           status: 404,
-//           statusText: ",Not Found",
-//           message: "The satellite '" + req.params.id + "' could not be found.",
-//           error: {
-//             code: "NOT_FOUND",
-//             message:
-//               "The satellite '" + req.params.id + "' could not be found.",
-//           },
-//         });
-//       }
-//     },
-//     function (err) {
-//       next(err);
-//     }
-//   );
-// });
-
-//Configure router so all routes are prefixed with /api/v1
-//app.use("/api/", router);
-//test for vercel deploy
-//after test this works. will need to change for 
-// app.get("/", (req, res) => {
-//     res.send("Express on Vercel");
-//   });
-  
-//   // Initialize server
-// app.listen(5000, () => {
-//     console.log("Running on port 5000.");
-//   });
-
-//Create server to listen on port 5000
-// var server = app.listen(5000, function () {
-//   console.log("Node server is running on http://localhose:5000..");
-// });
-
-//module.exports = server;
 
 app.use('/', router);
 
--- repos/satelliteRepo.js
+++ repos/satelliteRepo.js
@@ -6,19 +6,24 @@ let satelliteRepo = {
         type: "Communication",
         description:
           " Used for telecommunication and broadcasting, these satellites relay and amplify radio signals from Earth and retransmit them back to Earth",
-      },
+      imageUrl : "images/communication.jpg"
+        },
       {
         id: 2,
         type: "EarthObservation",
         description:
           "Also known as Earth remote sensing satellites, these satellites are used for environmental monitoring, meteorology, mapping, and other applications",
-      },
+        imageUrl: "images/earthObservation.jpg"
+        },
       {
         id: 3,
         type: "Astronomical",
         description: "Used to monitor and image space",
+        imageUrl : "images/astronomical.jpg"
       },
     ];
   },
+
+  
 };
 module.exports = satelliteRepo;
