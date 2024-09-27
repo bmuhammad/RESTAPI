b548d2a72e85b559278f94a068132d93a748caa9
Brian <briancmuhammad@yahoo.com>
2024-09-27 12:02:36 -0700

    rollllll

--- index.js
+++ index.js
@@ -30,60 +30,52 @@ router.get("/", function (req, res, next) {
 });
 
 //Create GET/id to return a single satellite
-// router.get("/:id", function (req, res, next) {
-//   satelliteRepo.getById(
-//     req.params.id,
-//     function (data) {
-//       if (data) {
-//         res.status(200).json({
-//           status: 200,
-//           statusText: "OK",
-//           message: "All satellites retrieved.",
-//           data: data,
-//         });
-//       } else {
-//         res.status(200).json({
-//           status: 404,
-//           statusText: ",Not Found",
-//           message: "The satellite '" + req.params.id + "' could not be found.",
-//           error: {
-//             code: "NOT_FOUND",
-//             message:
-//               "The satellite '" + req.params.id + "' could not be found.",
-//           },
-//         });
-//       }
-//     },
-//     function (err) {
-//       next(err);
-//     }
-//   );
-// });
+router.get("/:id", function (req, res, next) {
+  satelliteRepo.getById(
+    req.params.id,
+    function (data) {
+      if (data) {
+        res.status(200).json({
+          status: 200,
+          statusText: "OK",
+          message: "All satellites retrieved.",
+          data: data,
+        });
+      } else {
+        res.status(200).json({
+          status: 404,
+          statusText: ",Not Found",
+          message: "The satellite '" + req.params.id + "' could not be found.",
+          error: {
+            code: "NOT_FOUND",
+            message:
+              "The satellite '" + req.params.id + "' could not be found.",
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
 
 //Configure router so all routes are prefixed with /api/v1
 //app.use("/api/", router);
 //test for vercel deploy
 //after test this works.
-// app.get("/", (req, res) => {
-//     res.send("Express on Vercel");
-//   });
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
   
-//   // Initialize server
-// app.listen(5000, () => {
-//     console.log("Running on port 5000.");
-//   });
-const PORT = process.env.PORT || 3000;
-app.use('/api', router); // Mount the router at the '/api' path
-
-
-
-//Create server to listen on port 5000
-//   var server = app.listen(3000, function () {
-//     console.log("Node server is running on http://localhose:5000..");
-//   });
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  // Initialize server
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
   });
 
- module.exports = app;
+//Create server to listen on port 5000
+// var server = app.listen(5000, function () {
+//   console.log("Node server is running on http://localhose:5000..");
+// });
+
+//module.exports = server;
