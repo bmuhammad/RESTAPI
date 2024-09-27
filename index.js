d6c61726df5e7a6b111568acdd82271b8eb84f73
Brian <briancmuhammad@yahoo.com>
2024-09-27 10:20:21 -0700

    initialize project, added node, express, simple api for getting data from a file

---
+++ index.js
@@ -0,0 +1,71 @@
+// Add express server and create application
+let express = require("express");
+let app = express();
+let satelliteRepo = require("./repos/satelliteRepo");
+
+//Use the express Router opbject
+let router = express.Router();
+//possible sql  call for data
+///let satellites = satelliteRepo.get();
+
+//Create GET to return a list of items
+router.get("/", function (req, res, next) {
+  // res.send("Communication"); return on single piece of data
+  //can send status here res.status(206).send based on needs
+  /// res.send(satellites);
+
+  satelliteRepo.get(
+    function (data) {
+      res.status(200).json({
+        status: 200,
+        statusText: "OK",
+        message: "All satellites retrieved.",
+        data: data,
+      });
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+//Create GET/id to return a single satellite
+router.get("/:id", function (req, res, next) {
+  satelliteRepo.getById(
+    req.params.id,
+    function (data) {
+      if (data) {
+        res.status(200).json({
+          status: 200,
+          statusText: "OK",
+          message: "All satellites retrieved.",
+          data: data,
+        });
+      } else {
+        res.status(200).json({
+          status: 404,
+          statusText: ",Not Found",
+          message: "The satellite '" + req.params.id + "' could not be found.",
+          error: {
+            code: "NOT_FOUND",
+            message:
+              "The satellite '" + req.params.id + "' could not be found.",
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+//Configure router so all routes are prefixed with /api/v1
+app.use("/api/", router);
+
+//Create server to listen on port 5000
+var server = app.listen(5000, function () {
+  console.log("Node server is running on http://localhose:5000..");
+});
+
+module.exports = server;
---
+++ repos/satelliteRepo.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+const FILE_NAME = './assets/satellites.json'
+
+let satelliteRepo = {
+  get: function (resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+        if (err) {
+            reject(err);
+        }
+        else {
+            resolve(JSON.parse(data));
+        }
+    });
+  },
+  getById:function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+        if(err) {
+            reject(err)
+        }
+        else {
+            let satellite = JSON.parse(data). find(s => s.id ==id);
+            resolve(satellite);
+        }
+    })
+  }
+  
+};
+
+module.exports = satelliteRepo;
